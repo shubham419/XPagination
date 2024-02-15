d08d811806652354a4cb74fd530b20b01a75330e
shubham <thoratshubham161@gmail.com>
2024-02-15 13:55:58 +0530

    url corrected

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   async function getApiData() {
     try {
       const res = await fetch(
-        "https://gektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
       );
       const data = await res.json();
       arryData.current = data;
